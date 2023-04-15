a16c0f97d91d21ae74493f48f529e0e16e86c011
Agar Avarzed <aavarzed24@wooster.edu>
2023-04-15 14:29:38 -0400

    Second submission

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,26 +5,18 @@ import MobileNav from './MobileNav';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
-// const { mouseEnterHandler, mouseLeaveHandler } = useContext(CursorContext);
-
   return (
-    <header className='fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center'>
+    <header
+      className='fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center'
+      style={{ backgroundColor: '#ffebee' }} // added background color
+    >
       <div className='flex flex-col xl:flex-row lg:items-center w-full justify-between'>
         {/* logo */}
-        <Link
-   //       onMouseEnter={mouseEnterHandler}
-    //      onMouseLeave={mouseLeaveHandler}
-          to={'/'}
-          className='max-w-[200px]'
-        >
+        <Link to={'/'} className='max-w-[200px]'>
           <img src={Logo} alt='' />
         </Link>
         {/* nav - initially hidden - show on desktop mode */}
-        <nav
-          className='hidden xl:flex gap-x-12 font-semibold'
-       //   onMouseEnter={mouseEnterHandler}
-        //  onMouseLeave={mouseLeaveHandler}
-        >
+        <nav className='hidden xl:flex gap-x-12 font-semibold'>
           <Link
             to={'/'}
             className='text-[#696c6d] hover:text-primary transition'
